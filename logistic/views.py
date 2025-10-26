3f08efca2ef4fcafa1e0225af55111870d346ae6
nemoymoy <nemoymoy@gmail.com>
2025-10-26 17:58:08 +0400

    check final

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'ПРИВЕТ ИЗБРАННЫМ!!!!!'})
+    return Response({'message': 'ПРИВЕТ ИЗБРАННЫМ на Марсе!!!!!'})
